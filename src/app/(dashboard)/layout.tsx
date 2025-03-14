8e2b9bfafaaff28a117c1374b15509b66cfa6f9a
Bhupesh <bhupeshvyas202@gmail.com>
2025-03-14 12:57:45 +0530

    Update layout.tsx

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,10 +1,13 @@
-import { Navbar } from "@/components/Navbar";
-import { Sidebar } from "@/components/Sidebar";
+import { Suspense } from "react";
 
 import { UpdateTaskModal } from "@/features/tasks/components/UpdateTaskModal";
 import { CreatetaskModal } from "@/features/tasks/components/CreateTaskModal";
 import { CreateProjectModal } from "@/features/projects/components/CreateProjectModal";
 import { CreateWorkspaceModal } from "@/features/workspace/components/CreateWorkspaceModal";
+
+import { Navbar } from "@/components/Navbar";
+import { Sidebar } from "@/components/Sidebar";
+import { PageLoader } from "@/components/PageLoader";
 
 type Props = {
   children: React.ReactNode;
@@ -12,25 +15,27 @@ type Props = {
 
 function Dashboardlayout({ children }: Props) {
   return (
-    <div className="min-h-screen">
-      <CreateWorkspaceModal />
-      <CreateProjectModal />
-      <CreatetaskModal />
-      <UpdateTaskModal />
+    <Suspense fallback={<PageLoader />}>
+      <div className="min-h-screen">
+        <CreateWorkspaceModal />
+        <CreateProjectModal />
+        <CreatetaskModal />
+        <UpdateTaskModal />
 
-      <div className="flex h-full w-full">
-        <div className="h-full hidden lg:block lg:w-[264px] fixed left-0 top-0 overflow-y-auto">
-          <Sidebar />
-        </div>
+        <div className="flex h-full w-full">
+          <div className="h-full hidden lg:block lg:w-[264px] fixed left-0 top-0 overflow-y-auto">
+            <Sidebar />
+          </div>
 
-        <div className="h-full lg:pl-[264px] w-full">
-          <div className="max-w-screen-2xl mx-auto h-full">
-            <Navbar />
-            <main className="h-full w-full py-8 px-6">{children}</main>
+          <div className="h-full lg:pl-[264px] w-full">
+            <div className="max-w-screen-2xl mx-auto h-full">
+              <Navbar />
+              <main className="h-full w-full py-8 px-6">{children}</main>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
 
